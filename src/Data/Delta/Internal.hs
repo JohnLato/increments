53524b9235b8b897da9a33670c9a1357acc78a82
John Lato <jwlato@tsurucapital.com>
2012-11-06 16:23:24 +0800

    refactor and clean up Delta.Internal
    also add a few more instances.

--- src/Data/Delta/Internal.hs
+++ src/Data/Delta/Internal.hs
@@ -18,36 +18,64 @@ module Data.Delta.Internal (
 import GHC.Generics
 
 import Data.Beamable
-import Data.Maybe (isJust)
-
-instance DeltaC Bool where
-
-instance DeltaC () where
-    type Delta () = ()
-    delta () () = ()
-    applyDelta () () = ()
-
-newtype D_Int = D_Int (Maybe Int) deriving (Show, Generic)
-
-instance Beamable D_Int
 
 instance DeltaC Int where
-    type Delta Int = D_Int
-    delta a b | b == a = D_Int Nothing
-              | otherwise = D_Int (Just b)
-    applyDelta _ (D_Int (Just d)) = d
-    applyDelta a (D_Int Nothing)  = a
+    type Delta Int = DPrim Int
+    delta = dprimDelta
+    applyDelta = dprimApply
 
-instance DeltaC (Proxy p) where
-    type Delta (Proxy p) = ()
-    delta _ _ = ()
-    applyDelta a _ = a
+instance DeltaC Char where
+    type Delta Char = DPrim Char
+    delta = dprimDelta
+    applyDelta = dprimApply
 
-instance (DeltaC a, Changed (Delta a)) => DeltaC (Maybe a) where
+instance Eq x => DeltaC [x] where
+    type Delta [x] = DPrim [x]
+    delta = dprimDelta
+    applyDelta = dprimApply
 
-instance (DeltaC l, DeltaC r, Changed (Delta l), Changed (Delta r)) => DeltaC (Either l r) where
+-- derived instances
+instance DeltaC ()
+instance DeltaC Bool
+instance (DeltaC a, Changed (Delta a)) => DeltaC (Maybe a)
+instance (DeltaC l, DeltaC r, Changed (Delta l), Changed (Delta r))
+         => DeltaC (Either l r)
 
--- class (Changed (Delta a)) => DeltaC a where
+instance (DeltaC l, DeltaC r, Changed (Delta l), Changed (Delta r)) => DeltaC (l,r)
+
+-- ---------------------------------------------------------------------
+-- wrap primitive-ish types in DPrim, and send new values only if there's been
+-- a change
+
+data DPrim a =
+    DPrim a
+  | DPrim_NoChange
+  deriving (Eq, Show, Generic)
+
+dprimDelta :: Eq a => a -> a -> DPrim a
+dprimDelta a b
+    | b == a    = DPrim_NoChange
+    | otherwise = DPrim b
+
+dprimApply :: a -> DPrim a -> a
+dprimApply _ (DPrim a)        = a
+dprimApply a (DPrim_NoChange) = a
+
+instance Beamable a => Beamable (DPrim a)
+
+instance Changed (DPrim a) where
+    didChange (DPrim _)      = True
+    didChange DPrim_NoChange = False
+
+-- ---------------------------------------------------------------------
+-- Main user-visible classes
+
+-- | Determine if a Delta representation contains a real change.  Unchanging
+-- deltas may be omitted.
+class Changed a where
+    didChange :: a -> Bool
+
+-- | Calculate differences between data structures.
 class DeltaC a where
     type Delta a :: *
     -- slightly bogus, this only works because the generic param p is
@@ -58,21 +86,16 @@ class DeltaC a where
     default delta :: (Generic a, GDeltaC (Rep a), GChanged (GDelta (Rep a)), Delta a ~ GDelta (Rep a) x) => a -> a -> Delta a
     delta a b = gdelta (from a) (from b)
 
+    -- | Apply a delta to a value
     applyDelta :: a -> Delta a -> a
     default applyDelta :: (Generic a, GDeltaC (Rep a), GChanged (GDelta (Rep a)), Delta a ~ GDelta (Rep a) x) => a -> Delta a -> a
     applyDelta a d_a = to $ gapplyDelta (from a) d_a
 
-instance Changed () where
-    didChange _ = False
 
-instance Changed D_Int where
-    didChange (D_Int (Just _)) = True
-    didChange (D_Int Nothing)  = False
-
-class (GChanged (GDelta f)) => GDeltaC f where
-    type GDelta f :: * -> *
-    gdelta :: f a -> f a -> GDelta f a
-    gapplyDelta :: f a -> GDelta f a -> f a
+-- ---------------------------------------------------------------------
+-- proxy tagged types
+-- could use Data.Tagged, but those don't have generic instances, and Tagged
+-- has the parameters in the wrong order.
 
 data Proxy p = Proxy deriving (Show, Generic)
 
@@ -80,12 +103,12 @@ instance Beamable (Proxy p)
 instance Changed (Proxy p) where
     didChange _ = False
 
-newtype P2 a p = P2 (Maybe a) deriving (Show, Generic)
+newtype P2 a p = P2 a
+  deriving (Show, Generic)
 
 instance Beamable a => Beamable (P2 a p)
 instance Changed a => Changed (P2 a p) where
-    didChange (P2 (Just a)) = didChange a
-    didChange (P2 Nothing)  = False
+    didChange (P2 a)      = didChange a
 
 data PSum a b delta_a delta_b p =
     PSNeither
@@ -111,9 +134,11 @@ instance Changed (PProd a b p) where
     didChange _         = True
 
 instance (Beamable (a p), Beamable (b p)) => Beamable (PProd a b p)
+instance (Beamable (a p), Beamable (b p), Beamable (d_a p), Beamable (d_b p))
+         => Beamable (PSum a b d_a d_b p)
 
-class Changed a where
-    didChange :: a -> Bool
+-- ---------------------------------------------------------------------
+-- generic version of Changed
 
 class GChanged a where
     g_didChange :: a p -> Bool
@@ -121,8 +146,8 @@ class GChanged a where
 instance GChanged Proxy where
     g_didChange _ = False
 
-instance GChanged (P2 a) where
-    g_didChange (P2 v) = isJust v
+instance Changed a => GChanged (P2 a) where
+    g_didChange (P2 a)     = didChange a
 
 instance (GChanged d_a, GChanged d_b) => GChanged (PSum a b d_a d_b) where
     g_didChange (PSLeft d)  = g_didChange d
@@ -136,7 +161,13 @@ instance (GChanged a, GChanged b) => GChanged (PProd a b) where
     g_didChange (PPRight b) = g_didChange b
     g_didChange PPNeither   = False
 
-instance (Beamable (a p), Beamable (b p), Beamable (d_a p), Beamable (d_b p)) => Beamable (PSum a b d_a d_b p)
+-- ---------------------------------------------------------------------
+-- generic version of DeltaC
+
+class (GChanged (GDelta f)) => GDeltaC f where
+    type GDelta f :: * -> *
+    gdelta :: f a -> f a -> GDelta f a
+    gapplyDelta :: f a -> GDelta f a -> f a
 
 instance GDeltaC U1 where
     type GDelta U1 = Proxy
@@ -182,11 +213,8 @@ instance (GDeltaC a, GDeltaC b) => GDeltaC (a :+: b) where
 
 instance (DeltaC a, Changed (Delta a)) => GDeltaC (K1 i a) where
     type GDelta (K1 i a) = P2 (Delta a)
-    gdelta (K1 a) (K1 b) = case delta a b of
-                              d | didChange d -> P2 . Just $ d
-                                | otherwise   -> P2 Nothing
-    gapplyDelta (K1 a) (P2 (Just d_a)) = K1 $ a `applyDelta` d_a 
-    gapplyDelta (K1 a) (P2 Nothing)    = K1 a
+    gdelta (K1 a) (K1 b) = P2 $ delta a b
+    gapplyDelta (K1 a) (P2 d_a)     = K1 $ a `applyDelta` d_a
 
 -- this instance used for datatypes with single constructor only
 instance (GDeltaC a, Datatype d, Constructor c) => GDeltaC (M1 D d (M1 C c a)) where
