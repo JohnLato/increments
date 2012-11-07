7df8a276c6fc20b77dba665ba4a981f388bd1ec7
John Lato <jwlato@tsurucapital.com>
2012-11-07 11:20:03 +0800

    Data.Delta: add DeltaCnstr

--- src/Data/Delta.hs
+++ src/Data/Delta.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE ConstraintKinds #-}
+
+{-# OPTIONS_GHC -Wall #-}
 module Data.Delta (
   DeltaC (..)
 , Changed (..)
+, DeltaCnstr
 ) where
 
 import Data.Delta.Internal
+
+-- | A useful type constraint synonym for writing instances
+type DeltaCnstr a = (DeltaC a, Changed (Delta a))
